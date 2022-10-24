ef0de8cd4573c72e148d5a72859afd98575ece79
Replit user <>
2022-10-24 17:39:32 +0000

    test

--- wordle.py
+++ wordle.py
@@ -1,18 +1,28 @@
 import random
 
+
 class Wordle:
+    '''
+    Represents the game in progress.
+    '''
+
     def __init__(self, words):
         self.words = words
         self.answer = random.choice(self.words).strip()
 
     def choose_word(self):
+        '''
+        Chooses a random word from the words array
+        and updates the value of self.answer.
+        '''
         self.answer = random.choice(self.words).strip()
 
-    ''' Returns a list of flags
+    def compare_words(self, input_word):
+        ''' Returns a list of flags
         G: the letter is in the right position
         Y: the letter is included in the word but in another position
-        -: the letter is not in the word '''
-    def compare_words(self, input_word):
+        -: the letter is not in the word 
+        '''
         length = len(self.answer)
         flags = [0] * (length)
         result = [0] * (length)
@@ -30,16 +40,17 @@ class Wordle:
 
             i_answer = 0
             while i_answer < length:
-                if flags[i_answer] == "Y" or result[i_input] == "Y" or result[i_answer] == "G":
+                if flags[i_answer] == "Y" or result[i_input] == "Y" or result[
+                        i_answer] == "G":
                     i_answer += 1
                     continue
-                
+
                 if self.answer[i_answer] == input_word[i_input]:
                     flags[i_answer] = "Y"
                     result[i_input] = "Y"
                     break
                 i_answer += 1
-                
+
             if result[i_input] != "Y":
                 result[i_input] = "-"
             i_input += 1
